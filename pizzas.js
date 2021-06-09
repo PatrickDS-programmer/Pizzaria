9dbf78a8607b2ded2e103fabb35339c99370f0d3
Patrick Duarte <patrickichigo1475@gmail.com>
2021-06-08 21:53:07 -0300

    first commit

---
+++ pizzas.js
@@ -0,0 +1,15 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza1.png', price:28.00, sizes:['100g', '530g', '860g'], description:'A pizza clássica para os amantes de queijo, feito com o melhor prouto do mercado com um sabor inigualavel'},
+
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:30.50, sizes:['320g', '530g', '860g'], description:'A clássica pizza com um sabor nunca visto antes, ingredientes selecionados com o melhor da linguiça calabresa'},
+
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:38.45, sizes:['320g', '530g', '860g'], description:'Essa pizza é a perfeita para os amantes de queijo, com 4 tipos diferentes dos melhores queijos do mundo direto para sua boca'},
+
+    {id:4, name:'Americana', img:'images/pizza4.png', price:35.77, sizes:['320g', '530g', '860g'], description:'O clássico sabor americano nunca visto antes, com novos ingredientes e um toque moderno'},
+
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:51.00, sizes:['320g', '530g', '860g'], description:'A inovação da pizza: pizza de sorvete ! mistura de quente e gelado com o clássico sabor de sorvete napolitano'},
+
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:38.55, sizes:['320g', '530g', '860g'], description:'Pizza criada pela casa, representa nossa raiz e hitória em uma redonda'},
+    
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:42.30, sizes:['320g', '530g', '860g'], description:'Quando pensamos em pizza doce nos vem a cabeça logo o chocolate, a clássica combinação de doce e salgado com o melhor chocolate nacional'}
+];
